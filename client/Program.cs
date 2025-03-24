75316ed488e65c835bb0a463a9d697ce71e779d2
Aortus <thijs1506@gmail.com>
2025-03-24 14:50:50 +0100

    client side program start

--- client/Program.cs
+++ client/Program.cs
@@ -30,22 +30,39 @@ class ClientUDP
 {
 
     //TODO: [Deserialize Setting.json]
+    
     static string configFile = @"../Setting.json";
     static string configContent = File.ReadAllText(configFile);
     static Setting? setting = JsonSerializer.Deserialize<Setting>(configContent);
 
-
     public static void start()
     {
-
         //TODO: [Create endpoints and socket]
+        int port = setting.ServerPortNumber;
+        string serverIp = setting.ServerIPAddress;
 
+        using (Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
 
-        //TODO: [Create and send HELLO]
+            string message = "HELLO";
+            byte[] data = Encoding.UTF8.GetBytes(message);
 
-        //TODO: [Receive and print Welcome from server]
+            udpSocket.SendTo(data, serverEndPoint);
+            Console.WriteLine($"Sent: {message} to {serverIp}:{port}");
 
+            udpSocket.ReceiveTimeout = 5000;
+            byte[] buffer = new byte[1024];
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            int received = udpSocket.ReceiveFrom(buffer, ref remoteEP);
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, received);
+            Console.WriteLine($"Received: {receivedMessage} from {remoteEP}");
+        }
         // TODO: [Create and send DNSLookup Message]
+        string dnsrecords = @"../DNSRecords.json";
+        string dnsrecordsContent = File.ReadAllText(dnsrecords);
+        DNSRecord[] dnsRecords = JsonSerializer.Deserialize<DNSRecord[]>(dnsrecordsContent);
+        
 
 
         //TODO: [Receive and print DNSLookupReply from server]
