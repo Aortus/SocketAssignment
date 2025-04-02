86b069f41b23c0bee5547fdd56f3161dbe475fe0
Pieter Smit <1058663@hr.nl>
2025-04-02 20:57:04 +0200

    server side content

--- server/Program.cs
+++ server/Program.cs
@@ -67,34 +67,42 @@ class ServerUDP
             int receivedBytes = serverSocket.ReceiveFrom(buffer, ref remoteEP);
             string receivedJson = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
             Message receivedMessage = JsonSerializer.Deserialize<Message>(receivedJson);
+            Console.WriteLine($"Received Content Type: {receivedMessage.Content.GetType()}");
+            Console.WriteLine($"Received message: Type={receivedMessage.MsgType} {receivedMessage.Content}");
 
-            Console.WriteLine($"Received message: Type={receivedMessage.MsgType}");
-                
             Message responseMessage = null;
+
             
             switch (receivedMessage.MsgType)
             {
                 case MessageType.Hello:
-                    responseMessage = new Message { MsgId = receivedMessage.MsgId, MsgType = MessageType.Welcome, Content = "Welcome!" };
+                    responseMessage = new Message { MsgId = 2, MsgType = MessageType.Welcome, Content = "Welcome!" };
                     break;
                 case MessageType.DNSLookup:
-                    string domainName = receivedMessage.Content?.ToString().Trim();
-                    Console.WriteLine(domainName);
-                    var dnsRecords = ReadDNSRecords();
-                    foreach(DNSRecord item in dnsRecords)
+                    if (receivedMessage.Content is JsonElement contentElement)
                     {
-                        Console.WriteLine(item.Name);
-                        if (item.Name.Trim() == domainName)
-                        {
-                            responseMessage = new Message { MsgId = receivedMessage.MsgId, MsgType = MessageType.DNSLookupReply, Content = item };
-                            Console.WriteLine("MESSAGE MADEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEee");
-                            break;
-                        }
-                    }
+                        DNSRecord dnsRecord = JsonSerializer.Deserialize<DNSRecord>(contentElement.GetRawText());
+                        string domainName = dnsRecord.Name;
+                        string recordType = dnsRecord.Type;
 
-                    if (responseMessage is null)
+                            var dnsRecords = ReadDNSRecords();
+                            foreach(DNSRecord item in dnsRecords)
+                            {
+                                Console.WriteLine(item.Name);
+                                if (item.Name == domainName && item.Type == recordType)
+                                {
+                                    responseMessage = new Message { MsgId = receivedMessage.MsgId, MsgType = MessageType.DNSLookupReply, Content = item };
+                                    break;
+                                }
+                            }
+                            if (responseMessage is null)
+                            {
+                                responseMessage = new Message { MsgId = receivedMessage.MsgId + 250000, MsgType = MessageType.Error, Content = "Domain not found" };            
+                            }
+                    }
+                    else
                     {
-                        responseMessage = new Message { MsgId = receivedMessage.MsgId, MsgType = MessageType.Error, Content = "Domain not found" };            
+                        responseMessage = new Message { MsgId = receivedMessage.MsgId, MsgType = MessageType.Error, Content = "Content is wrong"};
                     }
                     break;
                 case MessageType.End:
@@ -115,124 +123,8 @@ class ServerUDP
             }
             else
             {
-                Console.WriteLine("No response sent. Waiting for the next message...");
+                Console.WriteLine("Waiting for the next message...");
             }
         }
     }
-} 
-
-
-//                     if (receivedMessage.MsgType == MessageType.DNSLookup)
-//             {
-//                 string requestedDomain = receivedMessage.Content?.ToString() ?? "";
-//                 DNSRecord? foundRecord = ReadDNSRecords().FirstOrDefault(r => r.Name == requestedDomain);
-
-//                 if (foundRecord != null)
-//                 {
-//                     // Send DNSLookupReply
-//                     Message responseMessage = new Message
-//                     {
-//                         MsgId = receivedMessage.MsgId,
-//                         MsgType = MessageType.DNSLookupReply,
-//                         Content = foundRecord
-//                     };
-
-//                     SendResponse(serverSocket, remoteEP, responseMessage);
-//                 }
-//                 else
-//                 {
-//                     // Send Error: Domain not found
-//                     SendErrorMessage(serverSocket, remoteEP, "Domain not found");
-//                 }
-//             }
-
-// }
-
-
-
-        // TODO:[Receive and print DNSLookup]
-
-
-        // TODO:[Query the DNSRecord in Json file]
-
-        // TODO:[If found Send DNSLookupReply containing the DNSRecord]
-                // TODO:[Receive and print Hello]
-        
-                        // byte[] buffer = new byte[1000];
-                        // string data = null;
-
-                        // EndPoint remoteEP = (EndPoint)serverEndPoint;
-                        // int b = serverSocket.ReceiveFrom(buffer, ref remoteEP);
-                        // data = Encoding.ASCII.GetString(buffer, 0, b);
-                        // Console.WriteLine("A message received from "+ remoteEP.ToString() + " " + data);
-
-                // TODO:[Send Welcome to the client]
-
-
-        // TODO:[If not found Send Error]
-
-
-        // TODO:[Receive Ack about correct DNSLookupReply from the client]
-
-
-        // TODO:[If no further requests receieved send End to the client]
-
-    
-
-        // int port = setting.ServerPortNumber;
-        // string serverIP = setting.ServerIPAddress;
-
-
-        // byte[] buffer = new byte[1000];
-        // byte[] msg = Encoding.ASCII.GetBytes("From server: Your message delivered\n");
-
-        // string data = null;
-        // int MsgCounter = 0;
-        // IPAddress ipAddress = IPAddress.Parse(serverIP);
-        // IPEndPoint localEndpoint = new IPEndPoint(ipAddress, port);
-        // IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-        // EndPoint remoteEP = (EndPoint)sender;
-
-
-        // try
-        // {
-        //     Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        //     sock.Bind(localEndpoint);
-        //     while (MsgCounter < 10)
-        //     {
-        //         Console.WriteLine("\n Waiting for the next client message..");
-        //         int b = sock.ReceiveFrom(buffer, ref remoteEP);
-        //         data = Encoding.ASCII.GetString(buffer, 0, b);
-        //         Console.WriteLine("A message received from "+remoteEP.ToString()+ " " + data);
-        //         sock.SendTo(msg, msg.Length, SocketFlags.None, remoteEP);
-        //         MsgCounter++;
-        //     }
-        //     sock.Close();
-        // }
-        // catch
-        // {
-        //     Console.WriteLine("\n Socket Error. Terminating");
-        // }
-
-
-
-            // byte[] buffer = new byte[1000];
-            // byte[] msg = Encoding.ASCII.GetBytes("From server: Your message delivered\n");
-            // string data = null;
-
-            // IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-            // IPEndPoint localEndpoint = new IPEndPoint(ipAddress, 32000);
-
-            // Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            // sock.Bind(localEndpoint);
-            // sock.Listen(5);
-
-            // Console.WriteLine("\nWaiting for clients...");
-            // Socket newSock = sock.Accept();
-
-            // while (true)
-            // {
-            //     int b = newSock.Receive(buffer);
-            //     data = Encoding.ASCII.GetString(buffer, 0, b);
-
-
+}
